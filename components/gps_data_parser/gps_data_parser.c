2a5a074370244da2946e03bb8fb147a0fdf6bec3
Kanuz <kanuzkhan@gmail.com>
2023-09-12 17:50:42 +0500

    Adding GGA parsing function

--- components/gps_data_parser/gps_data_parser.c
+++ components/gps_data_parser/gps_data_parser.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "gps_data_parser.h"
 
 
@@ -24,10 +25,9 @@ bool check_sentence_validity_and_integrity(const char* gps_sentence)
     // Initial check for valid sentence.
     if (*gps_sentence++ != '$')
     {
-        printf("Invalid sentence type");
         return false;
     }else{
-        printf("valid sentence");
+        // do nothing
     }
 
     // Checksum for Packet integrity.
@@ -64,16 +64,240 @@ bool check_sentence_validity_and_integrity(const char* gps_sentence)
         }
     }
 
-    // To check of there is any other garbage value after checksum
+    // Check for terminating sequence
     while (*gps_sentence == '\r' || *gps_sentence == '\n') {
         gps_sentence++;
     }
 
-    // Extra check to see if there are still values after end sequence if yes then its invalid  
+    // Check to see if there are still values after end sequence if yes then its invalid  
     if (*gps_sentence) {
         return false;
     }else {
         // do nothing
+    }
+
+    return true;
+}
+bool sentence_type_GGA(const char *gps_sentence)
+{
+    // Check if the sentence is a GGA sentence
+    if (strncmp(gps_sentence, "$GPGGA,", 7) == 0){
+        return true;
+    }else{
+        return false;
+    } 
+}
+
+bool parse_GGA_sentence(const char *gps_sentence, GGA_Data_t *Received_data)
+{
+    // Check for validity and integrity
+    if (!( check_sentence_validity_and_integrity(gps_sentence) )){
+        return false;
+    }else{
+        // do nothing
+    }
+
+    if (!(sentence_type_GGA(gps_sentence))){
+        return false;
+    }else{
+        // do nothing
+    }
+
+    // Parsing the GGA sentence
+    sentence_gga_parameters_t field_parsed = 0;
+    const char *field_start = gps_sentence + 7;  // Pointing to the start of second field Skipping '$GPGGA,'
+    const char *field_end = NULL;
+    while (*field_start && (field_parsed < 15)){
+        int degrees_lat_lon = 0;
+
+        field_end = strchr(field_start, ',');  // To skip "," while extracting the required information
+        
+        // To make sure it points to the end of the field properly
+        if (field_end == NULL){
+            field_end = field_start + strlen(field_start);
+        }else{
+            // do nothing
+        }
+        
+
+        switch (field_parsed){
+            case GGA_FRAME_TIME:
+                if (!((field_end - field_start) >= 6)){
+                    Received_data->time.hours = 00;
+                    Received_data->time.minutes = 00;
+                    Received_data->time.seconds = 00.000;
+                }else{
+                    if ((sscanf(field_start, "%2hhd%2hhd%f", &Received_data->time.hours, &Received_data->time.minutes, &Received_data->time.seconds)) != 3){
+                        return false;
+                    }else {
+                        // do nothing
+                    }
+                }
+                break;
+
+            case GGA_LATITUDE_VALUE:
+                if (!((field_end - field_start) >= 4)){
+                    Received_data->latitude.latitude_val = 0000.0000;
+                }else{
+                // Extract degrees and minutes
+                    if (sscanf(field_start, "%2d%f", &degrees_lat_lon, &Received_data->latitude.latitude_val) != 2) {
+                        return false;
+                    }else{
+                        // Convert latitude to decimal degrees
+                        Received_data->latitude.latitude_val = degrees_lat_lon + (Received_data->latitude.latitude_val / 60.0);
+                    }
+                }
+                break;
+
+            case GGA_LATITUDE_INDICATOR: 
+                if ((*field_start != 'N') && (*field_start != 'S')){
+                    Received_data->latitude.indicator_lat = 0;
+                }else if (*field_start == 'S'){
+                    Received_data->latitude.indicator_lat = *field_start;
+                }else if (*field_start == 'N'){
+                    Received_data->latitude.indicator_lat = *field_start;
+                }else{
+                    // do nothing
+                }
+                break;
+
+            case GGA_LONGITUDE_VALUE: 
+                if (!((field_end - field_start) >= 5)){
+                    Received_data->longitude.longitude_val = 00000.0000;
+                }else{
+                // Extract degrees and minutes
+                    if (sscanf(field_start, "%3d%f", &degrees_lat_lon, &Received_data->longitude.longitude_val) != 2){
+                        return false;
+                    }else{
+                        // Convert longitude to decimal degrees
+                        Received_data->longitude.longitude_val = degrees_lat_lon + (Received_data->longitude.longitude_val / 60.0);
+                    }
+                }
+                break;
+
+            case GGA_LONGITUDE_INDICATOR:
+                if (*field_start != 'E' && *field_start != 'W'){
+                    Received_data->longitude.indicator_lon = 0;
+                }else if (*field_start == 'W'){
+                    Received_data->longitude.indicator_lon = *field_start;
+                }else if (*field_start == 'E'){
+                    Received_data->longitude.indicator_lon = *field_start;
+                }else{
+                    // do nothing
+                }
+                break;
+
+            case GGA_FIX_QUALITY:
+                if ((field_end - field_start) != 1 ){
+                    Received_data->fix_quality_indicator = 10;
+                }else{
+                    if (sscanf(field_start, "%hhd", &Received_data->fix_quality_indicator) != 1){
+                        return false;
+                    }else{
+                        // do nothing
+                    }
+                }
+                break;
+
+            case GGA_USED_SATELLITES:
+                if (!((field_end - field_start) <= 2 )){
+                    Received_data->satellites = 15;
+                }else{
+                    if (sscanf(field_start, "%hhd", &Received_data->satellites) != 1){
+                        return false;
+                    }else{
+                        // do nothing
+                    }
+                }
+                break;
+
+            case GGA_HDOP:
+                if (!((field_end - field_start) >= 3)){
+                    Received_data->horizontal_dilution_precision = 0.0;
+                }else{
+                    if (sscanf(field_start, "%f", &Received_data->horizontal_dilution_precision) != 1){
+                        return false;
+                    }else{
+                        // do nothing
+                    }
+                }
+                break;
+
+            case GGA_ALTITUDE:
+                if (!((field_end - field_start) >= 3)){
+                    Received_data->altitude.altitude_val = 0.0;
+                }else{
+                    if (sscanf(field_start, "%f", &Received_data->altitude.altitude_val) != 1){
+                        return false;
+                    }else{
+                        // do nothing
+                    }
+                }
+                break;
+
+            case GGA_ALTITUDE_UNIT:
+                if (*field_start != 'M'){
+                    Received_data->altitude.unit_altitude = 0;
+                }else if (*field_start == 'M'){
+                    Received_data->altitude.unit_altitude = *field_start;
+                }else{
+                    // do nothing
+                }
+                break;
+
+            case GGA_GEOID:
+                if (!((field_end - field_start) >= 3)){
+                    Received_data->geoidal_height.height_geoidal_separation = 0.0;
+                }else{
+                    if (sscanf(field_start, "%f", &Received_data->geoidal_height.height_geoidal_separation) != 1){ 
+                        return false;
+                    }else{
+                        // do nothing
+                    }
+                }
+                break;
+
+            case GGA_GEOID_UNIT:
+                if (*field_start != 'M'){
+                    Received_data->geoidal_height.unit_geoidal_height = 0;
+                }else if (*field_start == 'M'){
+                    Received_data->geoidal_height.unit_geoidal_height = *field_start;
+                }else{
+                    // do nothing
+                }
+                break;
+
+            case GGA_TIME_DGPS:
+                if (!((field_end - field_start) >= 1)){
+                    Received_data->differential_info.time_since_last_dgps = 0.0;
+                }else{
+                    if (sscanf(field_start, "%f", &Received_data->differential_info.time_since_last_dgps) != 1){ 
+                        return false;
+                    }else{
+                        // do nothing
+                    }
+                }
+                break;
+
+            case GGA_ID_STATION:
+                if (!((field_end - field_start) >= 3)){
+                    Received_data->differential_info.dgps_station_id = 9999;
+                }else{
+                    if (sscanf(field_start, "%hd", &Received_data->differential_info.dgps_station_id) != 1){
+                        Received_data->differential_info.dgps_station_id = 9999;
+                    }else{
+                        // do nothing
+                    }
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        // Move to the next field
+        field_parsed++;
+        field_start = field_end + 1;
     }
 
     return true;
--- components/gps_data_parser/gps_data_parser.h
+++ components/gps_data_parser/gps_data_parser.h
@@ -5,25 +5,48 @@
 extern "C" {
 #endif
 
-#define MAX_SENTENCE_LENGTH 80 // Maximum length of a nmea sentence to store it in an array of characters excluding   
+#include <stdbool.h>
+
+#define MAX_SENTENCE_LENGTH 80   // Maximum length of a nmea sentence to store it in an array of characters excluding terminating sequence
+
+typedef enum
+{
+    GGA_FRAME_TIME = 0,
+    GGA_LATITUDE_VALUE,
+    GGA_LATITUDE_INDICATOR,
+    GGA_LONGITUDE_VALUE,
+    GGA_LONGITUDE_INDICATOR,
+    GGA_FIX_QUALITY,
+    GGA_USED_SATELLITES,
+    GGA_HDOP,
+    GGA_ALTITUDE,
+    GGA_ALTITUDE_UNIT,
+    GGA_GEOID,
+    GGA_GEOID_UNIT,
+    GGA_TIME_DGPS,
+    GGA_ID_STATION,
+    GGA_CHECKSUM,
+    GGA_ERROR
+}sentence_gga_parameters_t;
+
 typedef struct{
-    uint8_t   hours;                                   // Time in HH
-    uint8_t   minutes;                                 // Time in MM
-    float seconds;                                 // Time in SS.SSS
+    int8_t   hours;                               // Time in HH
+    int8_t   minutes;                             // Time in MM
+    float    seconds;                             // Time in SS.SSS
 }frame_time_t;
 
 typedef struct{
-    float latitude;                                // Latitude in decimal degrees (DDMM.MMMM)
-    char lat_indicator;                            // N for North, S for South
+    float latitude_val;                            // Latitude in decimal degrees
+    char  indicator_lat;                           // N for North, S for South
 }position_latitude_t;
 
 typedef struct{
-    float longitude;                               // Longitude in decimal degrees (DDDMM.MMMM)
-    char lon_indicator;                            // E for East, W for West
+    float longitude_val;                           // Longitude in decimal degrees
+    char  indicator_lon;                           // E for East, W for West
 }position_longitude_t;
 
 typedef struct{
-    float altitude;                                // Altitude above sea level
+    float altitude_val;                            // Altitude above sea level
     char unit_altitude;                            // Unit of altitude (M for meters)
 }altitude_data_t;
 
@@ -33,21 +56,26 @@ typedef struct{
 }geoidal_data_t;
 
 typedef struct{
-    float time_since_last_dgps;                    // Time since last DGPS update (seconds)
-    uint16_t dgps_station_id;                           // DGPS reference station ID
+    float time_since_last_dgps;                    // Time (seconds) since last DGPS updateNull when DGPS is not used
+    uint16_t dgps_station_id;                      // DGPS reference station ID
 }Differential_GPS_Data_t;
 
 typedef struct {
-    frame_time_t time;                             // Time in hour minutes seconds (HHMMSS.SSS) format
-    position_latitude_t  latitude;                 // Latitude in decimal degrees (DDMM.MMMM) along with indicator (North or South)
-    position_longitude_t longitude;                // Longitude in decimal degrees (DDMM.MMMM) along with indicator (Easth or West)
+    frame_time_t time;                             // Time in hour minutes seconds format
+    position_latitude_t  latitude;                 // Latitude in decimal degrees along with indicator (North or South)
+    position_longitude_t longitude;                // Longitude in decimal degrees along with indicator (Easth or West)
     uint8_t fix_quality_indicator;                 // 0 = Invalid, 1 = GPS_Fix, 2 = DGPS_Fix, 3 = not_applicable
     uint8_t satellites;                            // Number of satellites used to calculate positions
     float horizontal_dilution_precision;           // The current satellite geometry's effect on position accuracy.
     altitude_data_t altitude;                      // Altitude above sealevel along with its unit 
-    geoidal_data_t geoidal_height;                  // The difference between ellipsoid and sea level along with its unit
+    geoidal_data_t geoidal_height;                 // The difference between ellipsoid and sea level along with its unit
     Differential_GPS_Data_t differential_info;     // Provides the time since last DGPS update and the reference station id
 }GGA_Data_t;
+
+
+bool check_sentence_validity_and_integrity(const char* gps_sentence);
+bool sentence_type_GGA(const char *gps_sentence);
+bool parse_GGA_sentence(const char *gps_sentence, GGA_Data_t *Received_data);
 
 #ifdef __cplusplus
 }
